4f57d8c953dad02d6fd151b9b5be5cd7121b98ba
Chocolabear <scoopr1016@naver.com>
2026-01-11 23:37:09 +0900

    UI errr

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.EventSystems; // 필수
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Unit_UI : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject unitCanvas; // 미리 만들어둔 World Space Canvas
+
     void Start()
     {
-        
+        // 처음에는 UI를 꺼둡니다.
+        if (unitCanvas != null) unitCanvas.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    // 유닛(Collider2D 필요)을 클릭했을 때 호출
+    public void OnPointerClick(PointerEventData eventData)
     {
+        ToggleUI();
+    }
+
+    public void ToggleUI()
+    {
+        bool isActive = unitCanvas.activeSelf;
+        unitCanvas.SetActive(!isActive);
         
+        if (!isActive) {
+            // UI가 켜질 때 애니메이션이나 연출을 추가할 수 있습니다.
+            Debug.Log("UI 표시됨");
+        }
     }
 }
