112e0fdd6ea590b607cb3b44922e3c6f154eceb7
Chocolabear <scoopr1016@naver.com>
2026-01-16 23:26:53 +0900

    Ap 갯수 조정

--- Assets/Scripts/CustomPanel.cs
+++ Assets/Scripts/CustomPanel.cs
@@ -5,17 +5,23 @@ public class OptionPanel : MonoBehaviour
 {
     public TMP_InputField apInput;
 
-    void OnEnable()
-    {
-        apInput.text = GameSettings.MaxAP.ToString();
-    }
-
-    public void ApplyAP()
+    public void CloseWithApply()
     {
         if (int.TryParse(apInput.text, out int value))
         {
             GameSettings.MaxAP = Mathf.Max(1, value);
-            Debug.Log("옵션에서 설정한 Max AP: " + GameSettings.MaxAP);
+            Debug.Log("AP 적용됨: " + GameSettings.MaxAP);
         }
+        else
+        {
+            apInput.text = GameSettings.MaxAP.ToString();
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        apInput.text = GameSettings.MaxAP.ToString();
     }
 }
